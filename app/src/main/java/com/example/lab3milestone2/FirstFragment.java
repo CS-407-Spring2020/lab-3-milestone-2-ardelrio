de256eb50d6911e5297920733f12b71bd9a0fc34
ardelrio <alonsorafaeldelrio@gmail.com>
2020-02-15 12:32:34 -0600

    Lab 3 Milestone 2

---
+++ app/src/main/java/com/example/lab3milestone2/FirstFragment.java
@@ -0,0 +1,36 @@
+package com.example.lab3milestone2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstFragment extends Fragment {
+    View view;
+    Button firstButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        //Get the reference of Button
+        firstButton = (Button) view.findViewById(R.id.firstButton);
+
+        //Perform setOnClickListener on first Button
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //Display a message using a Toast
+                Toast.makeText(getActivity(), "First Fragment", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.lab3milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get the reference of buttons
+        firstFragment = (Button) findViewById(R.id.firstFragment);
+        secondFragment = (Button) findViewById(R.id.secondFragment);
+
+        //Perform setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        //Perform setOnClickListener on Second Button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment)
+    {
+        //Create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+
+        //Create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        //Replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
